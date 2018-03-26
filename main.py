5900af0d7b539371e2b1af03465d0b95eb4c6f65
anvesh <anveshkumar.perugu@gmail.com>
2018-03-26 00:23:43 -0400

    main file for creating instances

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Main Module"""
+
+from string_case import StringCase
+
+# creating instance of StringCase
+
+instance = StringCase()
+
+#printString in upper case by taking input from console
+
+print(instance.printString())
