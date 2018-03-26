4f2149d0c4c2bde16d84d35c521cc6b66c10de92
anvesh <anveshkumar.perugu@gmail.com>
2018-03-26 00:36:39 -0400

    Python function to generate a list and tuple from sequence of numbers from console

---
+++ sequence.py
@@ -0,0 +1,8 @@
+def number_sequence():
+	input_sequence=input("Enter a sequence of comma separated numbers:")
+	output_list=input_sequence.split(",")
+	print("List of given numbers:",output_list)
+	print("tuple of given numbers:",tuple(output_list))
+
+number_sequence()
+	
